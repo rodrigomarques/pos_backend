8fb16920e81ba272b238523971e60cbc4a3a76fb
Rodrigo Marques <marques.coti@gmail.com>
2023-05-06 16:13:08 -0300

    DynamoDB

--- src/models/Localizacao.js
+++ src/models/Localizacao.js
@@ -5,7 +5,21 @@ const uuid = require('uuid');
 //});
 //dynamoose.local();
 dynamoose.aws.ddb.local();
+//dynamoose.aws.ddb.local(http://localhost:8000)
 
+/*
+// Create new DynamoDB instance
+const ddb = new dynamoose.aws.ddb.DynamoDB({
+    "credentials": {
+        "accessKeyId": "AKID",
+        "secretAccessKey": "SECRET"
+    },
+    "region": "us-east-1"
+});
+
+// Set DynamoDB instance to the Dynamoose DDB instance
+dynamoose.aws.ddb.set(ddb);
+*/
 const localizacaoSchema = new dynamoose.Schema({
     _id: {
         type: String,
@@ -16,7 +30,7 @@ const localizacaoSchema = new dynamoose.Schema({
         type: String,
     },
     chave_fk:{
-        type: Integer,
+        type: number,
         required: true,
     },
     data:{
